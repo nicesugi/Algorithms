cb380b8b79af1c0ff1fb2f67bde1c5808e5a1865
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-09-26 13:18:20 +0900

    Feat/ 사칙연산

---
+++ BaekJoon/01_Input_Ouput_and_arithmetic_operations/app.py
@@ -0,0 +1,18 @@
+# 1001
+# a, b = input().split()
+# print(int(a)+int(b))
+
+# 1001
+# a, b = map(int,input().split())
+# print(a-b)
+
+# 10998
+# a, b = map(int,input().split())
+# print(a*b)
+
+# 1008
+# a, b = map(int,input().split())
+# print(a/b)
+
+# 10926
+# print(input()+'??!')
