def893c2102966f0f7c730d1abe34f3d32c84705
yoon,seul-gi <104303285+nicesugi@users.noreply.github.com>
2022-11-14 22:12:29 +0900

    Create find_a_share.py

---
+++ Programmers/Lv. 0/find_a_share.py
@@ -0,0 +1,8 @@
+def solution(num1: int, num2: int) -> int:
+    return num1 // num2
+  
+# or lambda form
+solution = lambda num1, num2: num1 // num2
+
+# or magic method
+solution = int.__floordiv__
