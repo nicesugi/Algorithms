ce0117b8bc07416e64766cf56996d4efcc142e24
yoon,seul-gi <104303285+nicesugi@users.noreply.github.com>
2022-11-17 00:24:16 +0900

    Update sum_of_fractions.py

--- Programmers/Lv. 0/sum_of_fractions.py
+++ Programmers/Lv. 0/sum_of_fractions.py
@@ -1,9 +1,12 @@
+# 분모의 곱을 공통분모로 하여 계산 후 최대공약수를 이용해 약분
 def solution(denum1: int, num1: int, denum2: int, num2: int) -> int:
     up = (num1 * denum2) + (num2 * denum1)
     down = num1 * num2
     for i in range(min(up, down), 0, -1):
-        if up % i == 0 and down % i == 0:
+        if (up % i == 0) and (down % i == 0):
             break
-    answer = [up // i, down // i]
+    up = up // i
+    down = down // i
+    answer = [up, down]
     
     return answer
