fefa7fc99c43c99af57b1f0a65aee169c5d551f0
yoon,seul-gi <104303285+nicesugi@users.noreply.github.com>
2022-11-17 15:06:46 +0900

    Update compare_numbers.py

--- Programmers/Lv. 0/compare_numbers.py
+++ Programmers/Lv. 0/compare_numbers.py
@@ -4,16 +4,19 @@ def solution1(num1: int, num2: int) -> int:
         return 1
     return -1
     
+    
 # 매개변수의 비교값을 bool로 판별
 def solution2(num1: int, num2: int) -> int:
     if (num1 == num2) == True:
         return 1
     return -1
   
+    
 # bool값을 2 or 0 의 수식 작성
 def solution3(num1: int, num2: int) -> int:
     answer = (num1 == num2) * 2 - 1
     return answer
+
 
 # or lambda form
 solution3 = lambda num1, num2: (num1 == num2) * 2 - 1
