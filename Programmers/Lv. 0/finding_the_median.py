851fb6f6f942565ef3a33a9942c1c117827143cd
yoon,seul-gi <104303285+nicesugi@users.noreply.github.com>
2022-11-17 15:06:15 +0900

    Update finding_the_median.py

--- Programmers/Lv. 0/finding_the_median.py
+++ Programmers/Lv. 0/finding_the_median.py
@@ -1,4 +1,13 @@
-def solution(array: list) -> list:
+# if문 없이 작성했지만, 원소가 2개일 경우([0.1]) 중앙값이 없는데도 나옴
+def solution1(array: list) -> list:
+    array.sort()
+    length = len(array) // 2
+
+    return array[length]
+
+
+# array를 나눈 값이 0이 아닐경우, 0일 경우의 if문 작성
+def solution2(array: list) -> list:
     array.sort()
     length = len(array)
     if (length % 2) != 0:
