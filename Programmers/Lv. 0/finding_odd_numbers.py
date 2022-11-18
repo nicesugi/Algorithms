1795f503a154c4eae9cefc6068f7962dad12e91b
yoon,seul-gi <104303285+nicesugi@users.noreply.github.com>
2022-11-19 01:08:48 +0900

    Update finding_odd_numbers.py

--- Programmers/Lv. 0/finding_odd_numbers.py
+++ Programmers/Lv. 0/finding_odd_numbers.py
@@ -1,6 +1,6 @@
 def solution(n: int) -> list:
     list = []
-    for i in range(1, n + 1, 2):
+    for i in range(1, n+1, 2):
         list.append(i)
 
     return list
@@ -9,3 +9,8 @@ def solution(n: int) -> list:
 # 같은 내용 짧게 줄여봄
 def solution(n: int) -> list:
     return [i for i in range(1, n+1, 2)]
+
+
+# 더 짧게 줄여봄
+def solution(n: int) -> list:
+    return list(range(1, n+1, 2))
