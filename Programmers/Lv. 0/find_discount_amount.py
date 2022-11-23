9a9b744b81797f5f8c0d251c5eee322690cc4d28
yoon,seul-gi <104303285+nicesugi@users.noreply.github.com>
2022-11-23 19:41:52 +0900

    Update find_discount_amount.py

--- Programmers/Lv. 0/find_discount_amount.py
+++ Programmers/Lv. 0/find_discount_amount.py
@@ -1,4 +1,4 @@
-def solution(price: int) -> int:
+def solution1(price: int) -> int:
     if price >= 500000:
         discounted_price = price * 0.8
     elif 500000 > price >= 300000:
@@ -9,3 +9,11 @@ def solution(price: int) -> int:
         discounted_price = price
 
     return int(discounted_price)
+
+
+def solution2(price: int) -> int:
+    discount_rates = {500000: 0.8, 300000: 0.9, 100000: 0.95, 0: 1}
+    for discount_price, discount_rate in discount_rates.items():
+        if price >= discount_price:
+
+            return int(price * discount_rate)
