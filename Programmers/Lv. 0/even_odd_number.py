f8bff0145049d7dd97da0b0e3838b02f8c3d821a
yoon,seul-gi <104303285+nicesugi@users.noreply.github.com>
2022-11-24 21:47:59 +0900

    Create even_odd_number.py

---
+++ Programmers/Lv. 0/even_odd_number.py
@@ -0,0 +1,29 @@
+def solution1(num_list):
+    even = []
+    odd = []
+    for i in num_list:
+        if i % 2 == 0:
+            even.append(i)
+        else:
+            odd.append(i)
+
+    return [len(even), len(odd)]
+
+
+def solution2(num_list):
+    answer = [0, 0]
+    for i in num_list:
+        if i % 2 == 0:
+            answer[0] += 1
+        else:
+            answer[1] += 1
+
+    return answer
+
+
+def solution3(num_list):
+    answer = [0, 0]
+    for i in num_list:
+        answer[i % 2] += 1
+
+    return answer
