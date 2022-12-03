bc9cd82ff98c6595fbb962f2b03d0aeae67bb45a
yoon,seul-gi <104303285+nicesugi@users.noreply.github.com>
2022-12-03 22:52:03 +0900

    Update number_of_cases.py

--- Programmers/Lv. 0/number_of_cases.py
+++ Programmers/Lv. 0/number_of_cases.py
@@ -1,7 +1,11 @@
-from itertools import *
+import math
+def solution2(balls: int, share: int) -> int:
+    return math.comb(balls, share)
 
-# 시간초과
-def solution(balls: int, share: int) -> int:
+
+# 시간초과 실패
+import itertools
+def solution1(balls: int, share: int) -> int:
     balls = "".join(map(str, ([i for i in range(1, balls + 1)])))
     answer = len(list(itertools.combinations(balls, share)))
     
