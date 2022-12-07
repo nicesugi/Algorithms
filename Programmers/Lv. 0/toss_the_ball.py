65f10a483a389d2abc8227753e804076626d151b
yoon,seul-gi <104303285+nicesugi@users.noreply.github.com>
2022-12-07 15:44:57 +0900

    Update toss_the_ball.py

--- Programmers/Lv. 0/toss_the_ball.py
+++ Programmers/Lv. 0/toss_the_ball.py
@@ -1,7 +1,10 @@
-def solution(numbers: list, k: int) -> int:
+def solution2(numbers: list, k: int) -> int:
+    return numbers[(2*(k-1)) % len(numbers)]
+
+
+def solution1(numbers: list, k: int) -> int:
     while len(numbers[0::2]) < k:
         numbers += numbers
         if len(numbers[0::2]) == k:
             break
-            
-    return numbers[0::2][k - 1]
+    return numbers[0::2][k-1]
